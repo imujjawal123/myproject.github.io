6c2dc955866e3cbc61c2c557f75b299649fdd41d
imujjawal123 <ujjawal.rajput1996@gmail.com>
2022-05-26 17:32:01 +0530

    main

--- admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  session_start();
-  if (isset($_SESSION['ADMIN_LOGIN']) && isset($_SESSION['ADMIN_EMAIL'])) {
-  	 unset($_SESSION);
-  	 session_destroy();
-  	 header('location:../admin-login');
-  }
-?>
